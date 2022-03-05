f8aba767d4f4d3ad9ccb87934e99895ca5a8dad6
humanrizki <humanrizki123@gmail.com>
2022-03-05 22:30:35 +0700

    baru nyoba trix editor dan menambahkan fitur update serta delete untuk comment

--- app/Http/Livewire/UserCommentPost.php
+++ app/Http/Livewire/UserCommentPost.php
@@ -4,28 +4,59 @@ namespace App\Http\Livewire;
 
 use App\Models\Comment;
 use Livewire\Component;
-
 class UserCommentPost extends Component
 {
     public $comment;
     public $post;
     public $comments;
+    public $editComment;
+    public $isEditComment;
+    public $idComment;
     protected $rules = [
         'comment' => 'required',
     ];
     public function mount(){
         $this->comments = Comment::where('post_id',$this->post->id)->get();
+        $this->isReplyComment = false;
     }
     public function sendComment(){
+        $this->comment = nl2br(htmlspecialchars($this->comment));
         $this->validate();
         Comment::create([
             'post_id'=>$this->post->id,
             'user_id'=>auth()->user()->id,
-            'comment'=> htmlspecialchars($this->comment)
+            'comment'=> $this->comment
         ]);
         $this->reset('comment');
     }
-    
+    public function sendDeleteComment($id){
+        if($this->isEditComment){
+            $this->isEdit($id);
+        } else {
+            $this->isEdit($id);
+        }
+        Comment::destroy($id);
+    }
+    public function sendEditComment($id){
+        Comment::find($id)->update([
+            'comment'=>nl2br(htmlspecialchars($this->editComment))
+        ]);
+        $this->editComment = null;
+        $this->isEditComment = false;
+        $this->idComment = null;
+    }
+    public function isEdit($id){
+
+        if($this->isEditComment){
+            $this->isEditComment = false;
+            $this->idComment = null;
+            $this->editComment = null;
+        } else {
+            $this->idComment = $id;
+            $this->isEditComment = true;
+            $this->editComment = str_replace('<br />','',Comment::find($id)->comment);
+        }
+    }
     public function render()
     {
         $this->comments = Comment::where('post_id',$this->post->id)->get();
--- resources/views/livewire/user-comment-post.blade.php
+++ resources/views/livewire/user-comment-post.blade.php
@@ -1,16 +1,15 @@
 <div>
-    {{-- Stop trying to control. --}}
     <div class="w-full my-3">
         <form wire:submit.prevent="sendComment" >
             <div class="w-full p-2 bg-blue-500 text-white rounded-t">Comment</div>
-            <textarea wire:model="comment" id="" cols="30" rows="5" class="w-full border-2 border-blue-400 rounded-b p-2 focus:ring-red-500">
+            <textarea wire:model="comment" cols="30" rows="5"  class="w-full border-2 border-blue-400 rounded-b p-2 focus:ring-red-500">
             </textarea>
             @error('comment')
                 <p class="text-red-500">{{ $message }}</p>
             @enderror
+
             <button class="w-max text-white bg-blue-400 p-2 font-medium rounded shadow hover:bg-blue-500" type="submit">Comment</button>
         </form>
-
     </div>
     <hr>
     <div class="w-full">
@@ -25,9 +24,47 @@
                         <img src="/storage/{{ $c->user->profile }}" alt="" class=" w-[60px] h-[60px] object-cover rounded-full">
                         @endif
                     </div>
-                    <div class="content-text">
-                        <p class="text-lg font-medium">{{ '@'.$c->user->username }}</p>
-                        <p class="ml-1">{!! $c->comment !!}</p>
+                    <div class="content-text w-full">
+                        <p class="text-lg font-medium">{{ '@'.$c->user->username }} 
+                            @if ($c->user->id == auth()->user()->id)
+                            <button type="button" class="text-base text-blue-500" wire:click.prevent="isEdit({{ $c->id }})">
+                                @if ($isEditComment)
+                                    @if ($idComment == $c->id)
+                                    Batal edit
+                                    @else
+                                    Edit
+                                    @endif
+                                @else
+                                    Edit
+                                @endif
+                            </button>
+                            |
+                            <button class="text-base text-red-500" type="button" wire:click.prevent="sendDeleteComment({{ $c->id }})">
+                                Delete
+                            </button>
+                            @endif
+                        </p>
+                        @if ($idComment == $c->id)
+                            @if ($isEditComment)
+                            <form wire:submit.prevent="sendEditComment({{ $c->id }})" class="w-full my-2">
+                                <div class="w-full p-2 bg-blue-500 text-white rounded-t">Edit comment</div>
+                                <textarea wire:model="editComment" id="" cols="30" rows="5" class="w-full border-2 border-blue-400 rounded-b p-2 focus:ring-red-500">
+                                </textarea>
+                                @error('editComment')
+                                    <p class="text-red-500">{{ $message }}</p>
+                                @enderror
+                                <button class="w-max text-white bg-blue-400 p-2 font-medium rounded shadow hover:bg-blue-500" type="submit">Edit</button>
+                            </form>
+                            @endif
+                        @else
+                            <p class="ml-1">{!! $c->comment !!}
+                            </p>
+                                @if (\Carbon\Carbon::parse($c->created_at) == \Carbon\Carbon::parse($c->updated_at))
+                                <p class="ml-1 text-sm text-gray-400">datetime : {{ \Carbon\Carbon::parse($c->created_at)->diffForHumans() }}</p>
+                                @else
+                                <p class="ml-1 text-sm text-gray-400">datetime : {{ \Carbon\Carbon::parse($c->created_at)->diffForHumans() }} | edit : {{ \Carbon\Carbon::parse($c->updated_at)->diffForHumans() }}</p>
+                                @endif
+                        @endif
                     </div>
                 </div>
             </div>
