0c4386abdd1c8d278a4b8d9f5ed9c184d93d68ec
Balq666 <balq2.0e@gmail.com>
2022-03-26 16:23:53 +0700

    benerin profile user

--- resources/views/user/profile/index.blade.php
+++ resources/views/user/profile/index.blade.php
@@ -57,7 +57,7 @@
                     </div>
                     <p class="text-lg font-normal text-gray-700 dark:text-gray-400">{{'@'.$user->username}}</p>
                     @if ($user->id == auth()->user()->id)
-                    <a href="/edit-profile" class="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                    <a href="/profile/{{auth()->user()->username}}/edit" class="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                         Edit
                         <svg class="ml-2 -mr-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                     </a>
@@ -100,7 +100,7 @@
                 </div>
 
                 <p class="mb-3 font-normal text-white dark:text-gray-400">Rp.{{number_format($balance)}}</p>
-                <a href="/topup" class="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white rounded-lg border border-white focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                <a href="/top-up/deposit" class="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white rounded-lg border border-white focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                     Topup
                     <svg class="ml-2 -mr-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                 </a>
